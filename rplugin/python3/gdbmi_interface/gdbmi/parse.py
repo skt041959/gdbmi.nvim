083ee348da7084f751aaec80a689dc7233493286
skt041959 <skt041959@gmail.com>
2016-09-28 22:13:53 +0800

    add list_parse

--- rplugin/python3/gdbmi_interface/gdbmi/parse.py
+++ rplugin/python3/gdbmi_interface/gdbmi/parse.py
@@ -3,15 +3,17 @@
 
 import re
 import collections
+from ast import literal_eval
+import logging
 
 TOKEN_NUM = r'(?P<TOKEN_NUM>^\d+)'
 RESULT_CLASS = r'\^(?P<RESULT_CLASS>(done|running|connected|error|exit))'
 EXEC_CLASS = r'\*(?P<EXEC_CLASS>[\w-]+)'
 STATUS_CLASS = r'\+(?P<STATUS_CLASS>[\w-]+)'
 NOTIFY_CLASS = r'=(?P<NOTIFY_CLASS>[\w-]+)'
-CONSOLE_OUTPUT = r'^~\"(?P<CONSOLE_OUTPUT>(.*?))\"'
-TARGET_OUTPUT = r'^@\"(?P<TARGET_OUTPUT>(.*?))\"'
-LOG_OUTPUT = r'^&\"(?P<LOG_OUTPUT>(.*?))\"'
+CONSOLE_OUTPUT = r'^~\"(?P<CONSOLE_OUTPUT>(.*?))\"(?=\n)'
+TARGET_OUTPUT = r'^@\"(?P<TARGET_OUTPUT>(.*?))\"(?=\n)'
+LOG_OUTPUT = r'^&\"(?P<LOG_OUTPUT>(.*?))\"(?=\n)'
 # L_TUPLE = r'(?P<L_TUPLE>\{)'
 # R_TUPLE = r'(?P<R_TUPLE>\})'
 TUPLE = r'\{(?P<TUPLE>(.*?))\}'
@@ -51,14 +53,18 @@ class GDBOutputParse:
 
     def __init__(self):
         self.GDB_PROMPT = object()
-        self.record_parse_func = {'RESULT_CLASS' : self.result_record,
-                                'EXEC_CLASS' : self.async_record,
-                                'STATUS_CLASS' : self.async_record,
-                                'NOTIFY_CLASS' : self.async_record,
-                                'CONSOLE_OUTPUT' : self.stream_record,
-                                'TARGET_OUTPUT' : self.stream_record,
-                                'LOG_OUTPUT' : self.stream_record
-                                }
+        self.record_parse_func = {
+            'RESULT_CLASS'  : self.result_record,
+            'EXEC_CLASS'    : self.async_record,
+            'STATUS_CLASS'  : self.async_record,
+            'NOTIFY_CLASS'  : self.async_record,
+            'CONSOLE_OUTPUT': self.stream_record,
+            'TARGET_OUTPUT' : self.stream_record,
+            'LOG_OUTPUT'    : self.stream_record,
+        }
+        self.string_parser = literal_eval
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.ERROR)
 
     @staticmethod
     def generate_tokens(text):
@@ -68,7 +74,7 @@ class GDBOutputParse:
             yield tok
 
     def parse(self, text):
-        self.tokens = self.generate_tokens(text)
+        self.tokens = GDBOutputParse.generate_tokens(text)
         self.tok = None # Last symbol consumed
         self.nexttok = None  # Next symbol tokenized
         self._advance()
@@ -77,14 +83,14 @@ class GDBOutputParse:
 
     def output_parse(self):
         if self._accept('GDB'):
-            return self.GDB_PROMPT
+            return None, self.GDB_PROMPT
 
         n = self.token_num()
         return n, self.record_parse()
 
     def token_num(self):
         if self._accept('TOKEN_NUM'):
-            return int(self.tok.value)
+            return self.tok.value
         else:
             return None
 
@@ -102,6 +108,42 @@ class GDBOutputParse:
                                          r'\"(?P<C>(.*?))\"',
                                          r'(?P<A>\,)',
                                          ]))
+        def list_parse(seq):
+            value_iter = value_pat.finditer(seq)
+            results = []
+            while 1:
+                m = next(value_iter, None)
+                if m is None:
+                    break
+                kind = m.lastgroup
+                if kind == 'V':
+                    var = m.group(kind)
+                    m = next(value_iter, None)
+                    if m is None:
+                        raise ParseError
+                    kind = m.lastgroup
+                    if kind == 'T':
+                        value = tuple_parse(m.group(kind))
+                    elif kind == 'L':
+                        value = list_parse(m.group(kind))
+                    elif kind == 'C':
+                        value = m.group(kind)
+                    else:
+                        raise ParseError
+                if kind == 'T':
+                    value = tuple_parse(m.group(kind))
+                if kind == 'C':
+                    value = m.group(kind)
+                results.append(value)
+                m = next(value_iter, None)
+                if m is None:
+                    break
+                elif m.lastgroup == 'A':
+                    continue
+                else:
+                    raise ParseError
+            return results
+
         def tuple_parse(seq):
             value_iter = value_pat.finditer(seq)
             results = {}
@@ -120,7 +162,7 @@ class GDBOutputParse:
                     if kind == 'T':
                         value = tuple_parse(m.group(kind))
                     elif kind == 'L':
-                        value = [e.strip()[1:-1] for e in m.group(kind).split(',')]
+                        value = list_parse(m.group(kind))
                     elif kind == 'C':
                         value = m.group(kind)
                     else:
@@ -144,33 +186,45 @@ class GDBOutputParse:
             if self._accept("TUPLE"):
                 value = tuple_parse(self.tok.value)
             elif self._accept("LIST"):
-                value = [e.strip()[1:-1] for e in self.tok.value.split(',')]
+                value = list_parse(self.tok.value)
             elif self._accept("CONST"):
                 value = self.tok.value
-            try:
-                results[var] = value
-            except:
-                raise ParseError
+            results[var] = value
         return results
 
     def result_record(self):
         result_class = self.tok.value
         results = self.results()
-        return ResultRecord('ResultRecord', result_class, results)
+        if self._accept('NL'):
+            return ResultRecord('ResultRecord', result_class, results)
+        else:
+            raise ParseError(repr(self.nexttok))
 
     def async_record(self):
         async_class = self.tok.type
         async_name = self.tok.value
         results = self.results()
-        return AsyncRecord('AsyncRecord', async_class, async_name, results)
+        if self._accept('NL'):
+            return AsyncRecord('AsyncRecord', async_class, async_name, results)
+        else:
+            raise ParseError(repr(self.tok) + repr(self.nexttok))
 
     def stream_record(self):
         stream_class = self.tok.type
-        output = self.tok.value
-        return StreamRecord('StreamRecord', stream_class, output)
+        try:
+            output = self.string_parser('"' + self.tok.value + '"')
+        except SyntaxError:
+            raise ParseError(repr(self.tok))
+
+        #  if self.nexttok is None:
+        if self._accept('NL'):
+            return StreamRecord('StreamRecord', stream_class, output)
+        else:
+            raise ParseError(repr(self.nexttok))
 
     def _advance(self):
         self.tok, self.nexttok = self.nexttok, next(self.tokens, None)
+        self.logger.debug(repr(self.tok))
 
     def _accept(self, toktype):
         if self.nexttok and self.nexttok.type == toktype:
@@ -180,17 +234,18 @@ class GDBOutputParse:
             return False
 
 
-def main():
+def main(filename):
     parser = GDBOutputParse()
-    with open('./test.txt') as f:
+    with open(filename) as f:
         for s in f:
-            s = s.strip()
-            print(">>" + s)
+            #  s = s.strip()
+            print(">>" + s.strip())
             #  for t in generate_tokens(s):
             #      print("  " + repr(t))
             print("  " + repr(parser.parse(s)))
 
 
 if __name__ == "__main__":
-    main()
+    import sys
+    main(sys.argv[1])
 
